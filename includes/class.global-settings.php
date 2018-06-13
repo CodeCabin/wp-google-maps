a8263f66849103bd1e04d91f43d8e0e276c76839
PerryRylance <perryrylance@googlemail.com>
2018-06-13 12:21:06 +0100

    7.10.13
    - Fixed store locator reset not working
    - Fixed disabling map controls not working
    - Fixed store locator radio button

---
+++ includes/class.global-settings.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace WPGMZA;
+
+class GlobalSettings
+{
+	private static $migrationRequired = false;
+	
+	private $data;
+	
+	public function __construct()
+	{
+		$this->reload();
+			
+		if(empty($this->data))
+			$this->migrate();
+	}
+	
+	public function reload()
+	{
+		$string = get_option('wpgmza_global_settings');
+		if(empty($string))
+			$this->data = (object)array();
+		else
+		{
+			$this->data = json_decode($string);
+			
+			if(!$this->data)
+				throw new \Exception('wpgmza_global_settings is not valid JSON');
+		}
+	}
+	
+	protected static function createInstanceDelegate()
+	{
+		//var_dump("It doesn't work");
+		//exit;
+		
+		return new GlobalSettings();
+	}
+	
+	public static function createInstance()
+	{
+		return static::createInstanceDelegate();
+	}
+
+	/**
+	 * Migrates old settings (< 7.11.*), merges WPGMZA_SETTINGS and WPGMZA_OTHER_SETTINGS into wpgmza_global_settings (JSON)
+	 * @return void
+	 */
+	private function migrate()
+	{
+		if(GlobalSettings::$migrationRequired)
+			return;
+		
+		$settings 			= get_option('WPGMZA_SETTINGS');
+		$other_settings		= get_option('WPGMZA_OTHER_SETTINGS');
+		
+		$json = json_encode( array_merge($settings, $other_settings) );
+		
+		update_option('wpgmza_global_settings', $json);
+		
+		$this->reload();
+	}
+	
+	
+}
